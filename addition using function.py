9fed3cc789657c18cecb716c074a7ecbcecee6fe
dkdeepak9908-coder <dkdeepak9908@gmail.com>
2026-03-15 19:53:30 +0530

    Create addition using function.py

---
+++ addition using function.py
@@ -0,0 +1,12 @@
+# Function to add two numbers
+def add(a, b):
+    return a + b
+
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Function call
+sum = add(num1, num2)
+
+print("Sum =", sum)
