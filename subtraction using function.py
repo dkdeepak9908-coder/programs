50557a9a20614ac080d86513e0c040de6644e18e
dkdeepak9908-coder <dkdeepak9908@gmail.com>
2026-03-15 19:57:49 +0530

    Create subtraction using function.py

---
+++ subtraction using function.py
@@ -0,0 +1,12 @@
+# Function to subtract two numbers
+def subtract(a, b):
+    return a - b
+
+# Input from user
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# Function call
+diff = subtract(num1, num2)
+
+print("Difference =", diff)
